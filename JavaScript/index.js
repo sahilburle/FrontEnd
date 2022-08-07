45a4cca2ddc57761d8b711bd65fcfbfeead6a081
sahilburle <sahilburle1998@gmail.com>
2022-08-07 14:54:35 +0530

    js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,5 +1,8 @@
-//Console API
-console.log("hello world")
-        // alert("hey");
+//  1. Console API
+console.log("hello world");
 
-        
+//  2. JS Variables
+var num1 = 10;
+var num2 = 12;
+
+console.log(num1 + num2);
