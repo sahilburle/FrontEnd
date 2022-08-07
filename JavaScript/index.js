5771ea1ce2d7d4c68c5c0ca637623b497045ba1f
sahilburle <sahilburle1998@gmail.com>
2022-08-07 18:03:53 +0530

    js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,10 +5,10 @@ console.log("hello world");
 var num1 = 10;
 var num2 = 12;
 
-console.log(num1 + num2);
+//console.log(num1 + num2);
 
 var str1 = "this is string";
-console.log(str1);
+//console.log(str1);
 
 //objects
 var marks = {
@@ -17,17 +17,17 @@ var marks = {
   harry: 70,
 };
 
-console.log(marks);
+//console.log(marks);
 
 var arr = [1, 2, 3, 4, 5];
 
-function avg(a, b) {
-  c = (a + b) / 2;
-  return c;
-}
+// function avg(a, b) {
+//   c = (a + b) / 2;
+//   return c;
+// }
 
-c1 = avg(4, 6);
-c2 = avg(14, 16);
+// c1 = avg(4, 6);
+// c2 = avg(14, 16);
 
 // arr.forEach(function (element) {
 //   console.log(element);
@@ -36,9 +36,29 @@ c2 = avg(14, 16);
 
 let j = 0;
 while(j < arr.length){
-    console.log(arr[j]);
+    //console.log(arr[j]);
     j++;
 }
 
 let myArr = ["Fan", 34, null, true];
-console.log(myArr.length);
+// console.log(myArr.length);
+
+// console.log(myArr.toString());
+
+//String methods
+
+let myStr = "harry is teaching";
+
+// console.log(myStr.length);
+// console.log(myStr.indexOf("is"));
+
+myDate = new Date();
+console.log(myDate.getHours());
+
+//  DOM Manipulation
+let elem = document.getElementById('click');
+console.log(elem);
+
+let elemClass = document.getElementsByClassName('container');
+console.log(elemClass);
+elemClass[0].style.background = "yellow";
