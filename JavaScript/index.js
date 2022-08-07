ef7710065c52c9f19a236c0e0a8123f9bd86c13b
sahilburle <sahilburle1998@gmail.com>
2022-08-07 17:21:56 +0530

    js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -7,28 +7,37 @@ var num2 = 12;
 
 console.log(num1 + num2);
 
-var str1 = 'this is string';
+var str1 = "this is string";
 console.log(str1);
 
 //objects
-var marks ={
-    ravi: 50,
-    shubham: 60,
-    harry: 70
-}
+var marks = {
+  ravi: 50,
+  shubham: 60,
+  harry: 70,
+};
 
 console.log(marks);
 
-var arr = [1,2,3,4,5]
+var arr = [1, 2, 3, 4, 5];
 
-function avg(a, b){
-    c = (a + b)/2;
-    return c;
+function avg(a, b) {
+  c = (a + b) / 2;
+  return c;
 }
 
 c1 = avg(4, 6);
 c2 = avg(14, 16);
 
-arr.forEach(function(element){
-    console.log(element)
-})
+// arr.forEach(function (element) {
+//   console.log(element);
+// });
+
+
+let j = 0;
+while(j < arr.length){
+    console.log(arr[j]);
+    j++;
+}
+
+let myArr = ["Fan", 34, null, true];
